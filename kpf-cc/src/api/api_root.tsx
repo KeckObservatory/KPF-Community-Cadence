832eadb7153268654d5d9b14be3151cf87e462eb
Tyler Tucker <tylertucker202@gmail.com>
2024-03-15 10:24:58 -1000

    typo

--- kpf-cc/src/api/api_root.tsx
+++ kpf-cc/src/api/api_root.tsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { handleResponse, handleError, intResponse, intError } from './response';
 import { Target } from '../App';
 const SIMBAD_ADDR = "https://simbad.u-strasbg.fr/simbad/sim-id?NbIdent=1&submit=submit+id&output.format=ASCII&obj.bibsel=off&Ident="
-const API_ADDR = "https://www3build.keck.hawaii.edu/api/proposals"
-//const API_ADDR = "/api/proposals"
+//const API_ADDR = "https://www3build.keck.hawaii.edu/api/proposals"
+const API_ADDR = "/api/proposals"
 
 
 export interface UserInfo {
