f00e1ad13aa4adcaebaf66f2535e31033be5d52c
Tyler Tucker <tylertucker202@gmail.com>
2024-04-16 09:49:55 -1000

    remove unused icon

--- kpf-cc/src/markdown_dialog.tsx
+++ kpf-cc/src/markdown_dialog.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
-import HelpIcon from '@mui/icons-material/Help';
 import IconButton from '@mui/material/IconButton';
 import DialogContent from '@mui/material/DialogContent';
 import Tooltip from '@mui/material/Tooltip';
