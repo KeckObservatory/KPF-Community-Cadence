cd0f5be31b87cb38d19a20f8a666a4780302135f
Tyler Tucker <tylertucker202@gmail.com>
2025-01-13 10:45:28 -1000

    ob save look

--- src/ob_component_table.tsx
+++ src/ob_component_table.tsx
@@ -236,6 +236,7 @@ export default function OBComponentTable(props: Props) {
         if (!newOb) return
         newOb = { ...newOb, [componentName]: row }
         const resp = await save_obs([newOb])
+        console.log('obsering block saved response', resp)
         if (!resp.observing_blocks) {
             console.error('save failed', resp)
             snackbarContext.setSnackbarMessage(
