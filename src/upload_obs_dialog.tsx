506a23ef203eef20ce780702ca4baa62761e1251
Tyler Tucker <tylertucker202@gmail.com>
2025-01-17 11:28:46 -1000

    convert translator OBS into obs on import

--- src/upload_obs_dialog.tsx
+++ src/upload_obs_dialog.tsx
@@ -7,7 +7,6 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { Tooltip } from '@mui/material';
 import UploadIcon from '@mui/icons-material/Upload';
-import { OBTarget } from './module_selector';
 import { ob_schemas } from './validation_check_dialog';
 import { JSONSchema7 } from 'json-schema';
 
