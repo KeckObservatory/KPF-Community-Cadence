14ca4a34c6c3f4244d4eb66ab4974f20f850cfa6
Tyler Tucker <tylertucker202@gmail.com>
2025-01-21 08:57:32 -1000

    lng input

--- src/ob_component_table.tsx
+++ src/ob_component_table.tsx
@@ -518,7 +518,7 @@ export default function OBComponentTable(props: Props) {
         const valid = errors.length === 0
 
         const ob = context.obs.find((ob) => ob._id === id)
-        console.log('ob action metadata', ob.metadata)
+        console.log('ob action metadata', ob?.metadata)
 
         const firstButton = valid ?
             <Tooltip
