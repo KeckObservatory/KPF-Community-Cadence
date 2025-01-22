388eeae5f89a81812f9fc05440bf76b6293af5da
Tyler Tucker <tylertucker202@gmail.com>
2025-01-21 16:49:38 -1000

    remove bib section check

--- src/catalog_button.tsx
+++ src/catalog_button.tsx
@@ -107,7 +107,7 @@ export const get_simbad_data = async (targetName: string): Promise<SimbadTargetD
         }
         else if(identifiersSection) //only check if in identifiers section
         {
-            console.log('line', line)
+            console.log('line', line, simbadData)
             if (simbadData.tic_id)  simbadData['tic_id'] = line.match(new RegExp('TIC\\s(\\w+)'))?.at(1)
             if (simbadData.two_mass_id)  simbadData['two_mass_id'] = line.match(new RegExp('2MASS\\s(\\w+)'))?.at(1)
             if (simbadData.gaia_id) {
