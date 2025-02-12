93e4a9cf35189cf33cb14d949be208e60ae430f5
Tyler Tucker <tylertucker202@gmail.com>
2025-02-11 16:54:30 -1000

    adding regex to check if positive

--- src/ob_edit_util.tsx
+++ src/ob_edit_util.tsx
@@ -276,7 +276,7 @@ export const make_text_field = (
         console.warn('make_text_field', `key ${key} not in component ${componentName}`)
         return
     }
-    const shrinkInputLabel = (value==null || value==undefined || value=="")? false : true 
+    const shrinkInputLabel = (value !== 0 || value==null || value==undefined || value=="")? false : true 
     return (
         <Tooltip title={input_label(key, componentName, true)}>
             <TextField
