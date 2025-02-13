c56006ac47f9b329459bd43fcf79e98fb6a8e883
Tyler Tucker <tylertucker202@gmail.com>
2025-02-13 09:12:14 -1000

    needInitSubmit logic

--- src/ob_component_table.tsx
+++ src/ob_component_table.tsx
@@ -509,7 +509,7 @@ export default function OBComponentTable(props: Props) {
         }
         const valid = errors.length === 0
         const publishColor = needsResubmit? 'inherit' : 'success'
-        const needsInitSubmit = needsResubmit && editRow.submitted === false
+        const needsInitSubmit = needsResubmit && !editRow.submitted
 
 
         const firstButton = valid ?
@@ -519,14 +519,14 @@ export default function OBComponentTable(props: Props) {
                 arrow key="publish" >
                 <GridActionsCellItem
                     disabled={!valid}
-                    icon={needsInitSubmit?
+                    icon={needsResubmit && !needsInitSubmit?
+                        <RefreshIcon
+                            sx={refreshStyle}
+                            color='warning' /> :
                         <PublishIcon
                             sx={refreshStyle}
                             color={publishColor}
-                        /> :
-                        <RefreshIcon
-                            sx={refreshStyle}
-                            color='warning' /> 
+                        />
                     }
                     label="Publish"
                     onClick={() => handlePublishClick(id, setIconSpin, setRowWithCadenceChecks)}
