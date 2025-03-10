586edc65e3cfc6bd084ec0f3b10a51973475598e
Tyler Tucker <tylertucker202@gmail.com>
2025-03-10 11:31:24 -1000

    upload forces obs to use semester, obsid, observer_name

--- src/upload_obs_dialog.tsx
+++ src/upload_obs_dialog.tsx
@@ -65,6 +65,12 @@ export function UploadComponent(props: UploadProps) {
                     throw new Error(msg)
                 }
             })
+            ob.metadata = {
+                ...ob.metadata,
+                observer_name: context.username,
+                semester: context.semester,
+                obsid: context.obsid,
+            }
             // check that semid correct semid is present
             if (!context.semid.includes(ob.metadata.semid)) {
                 const msg = `OB for target ${ob.target?.target_name} ${ob.metadata.semid} does not equal selected semid ${context.semid}`
