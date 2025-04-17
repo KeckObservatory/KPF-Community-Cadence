ce5c3641ccced157b96d546c0275d9dbd85ce0b9
Tyler Tucker <tylertucker202@gmail.com>
2025-04-17 13:41:30 -1000

    disable until submit/delete all change is ready

--- src/ob_component_table.tsx
+++ src/ob_component_table.tsx
@@ -485,7 +485,7 @@ export default function OBComponentTable(props: Props) {
                 rows={rows ?? []}
                 getRowId={(row) => row._id}
                 editMode={'row'}
-                checkboxSelection={true}
+                // checkboxSelection={true} //TODO: uncomment when ready to release submit/delete all button 
                 processRowUpdate={processRowUpdate}
                 columns={columns}
                 rowModesModel={rowModesModel}
