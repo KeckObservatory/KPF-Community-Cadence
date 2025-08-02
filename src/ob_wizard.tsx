ec175b273d24376e064be832db264ed15b5d309a
Tyler Tucker <tylertucker202@gmail.com>
2025-08-01 14:36:45 -1000

    upload tooltip has more options

--- src/ob_wizard.tsx
+++ src/ob_wizard.tsx
@@ -310,7 +310,7 @@ export const OBWizardButton = () => {
     };
     return (
         <div>
-            <Tooltip title="Upload OBs from .json file">
+            <Tooltip title="Upload OBs from .json file. \nNote: JSON file expects a list of OBs separated by commas. \nex: [{ob1}, {ob2}, ...]">
                 <Button onClick={handleClickOpen} startIcon={<UploadIcon />}>
                     Upload OBs
                 </Button>
