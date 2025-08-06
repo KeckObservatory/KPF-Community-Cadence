5e50e8da54762276b04ca9f0bc97eb15a20bfc62
Tyler Tucker <tylertucker202@gmail.com>
2025-08-06 13:18:38 -1000

    check if is number in validation section

--- src/validation_check_dialog.tsx
+++ src/validation_check_dialog.tsx
@@ -33,7 +33,7 @@ export interface Props {
 }
 
 const strNumberFormat : FormatDefinition<number | string> = {
-  validate: (_: number | string) => { return true },
+  validate: (val: number | string) => { return !isNaN(val as number)},
   compare: (a: number | string, b: number | string) => {
     if (!(a && b)) return undefined
     return Number(a) >= Number(b) ? 1 : -1
