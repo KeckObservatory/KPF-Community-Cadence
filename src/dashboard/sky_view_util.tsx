ac99d08f6b272de3e9af737ef713d03fb6505514
Tyler Tucker <tylertucker202@gmail.com>
2025-09-15 15:09:24 -1000

    add dashboard items

---
+++ src/dashboard/sky_view_util.tsx
@@ -0,0 +1,254 @@
+import { VizRow } from './dome_chart.tsx'
+import {
+    RADIUS_EARTH,
+    ATMOSPHERE_HEIGHT,
+    ROUND_MINUTES,
+    STEP_SIZE,
+    TIMES_START,
+    TIMES_END,
+    GeoModel,
+    LngLatEl
+} from './constants'
+import { alt_az_observable } from './target_viz_chart.tsx';
+import { TargetView } from './dome_chart.tsx';
+import dayjs  from 'dayjs';
+
+export const hidate = (date: Date, timezone: string) => {
+    return dayjs(date).tz(timezone)
+}
+
+
+export const colors = [
+    '#1f77b4',  // muted blue
+    '#ff7f0e',  // safety orange
+    '#2ca02c',  // cooked asparagus green
+    '#d62728',  // brick 
+    '#9467bd',  // muted purple
+    '#8c564b',  // chestnut brown
+    '#e377c2',  // raspberry yogurt pink
+    '#7f7f7f',  // middle gray
+    '#bcbd22',  // curry yellow-green
+    '#17becf'   // blue-teal
+];
+
+
+export const date_to_juld = (date: Date) => {
+    return (date.getTime() / 86400000) + 2440587.5 //do not offset
+}
+
+export const get_gmt = (date?: Date) => {
+    //NOTE: This uses the 2000.00 equinox. This is the same as the GMST at 0h UT on 1 January 2000
+    if (!date) date = new Date()
+    const JD = date_to_juld(date)
+    const T = (JD - 2451545) / 36525;
+    let ThetaGMST = 67310.54841 + (876600 * 3600 + 8640184.812866) * T 
+    + .093104 * (T**2) - ( 6.2 * 10**-6 ) * ( T**3 )
+    ThetaGMST = ( ThetaGMST % ( 86400 * ( ThetaGMST / Math.abs(ThetaGMST) ) ) / 240) % 360
+    return ThetaGMST 
+}
+
+export const ra_dec_to_deg = (time: string, dec = false) => {
+    let [hours, min, sec] = time.split(':')
+    let deg
+    if (dec) {
+        let sign = 1
+        if (hours[0] === '+') hours = hours.substring(1);
+        if (hours[0] === '-') {
+            hours = hours.substring(1);
+            sign = -1;
+        }
+        deg = sign * (parseInt(hours, 10) // dec is already in degrees
+            + parseInt(min, 10) / 60
+            + parseInt(sec, 10) / 60 ** 2)
+    }
+
+    else {
+        deg = 15 * parseInt(hours, 10) // convert hours to deg
+            + 15 * parseInt(min, 10) / 60
+            + 15 * parseInt(sec, 10) / 60 ** 2
+    }
+    return deg
+}
+
+export const d2r = (deg: number) => {
+    return deg * Math.PI / 180
+}
+
+export const r2d = (rad: number) => {
+    return rad / d2r(1) 
+}
+
+export const cosd = (deg: number): number => {
+    return Math.cos(d2r(deg))
+}
+
+export const sind = (deg: number): number => {
+    return Math.sin(d2r(deg))
+}
+
+export const tand = (deg: number): number => {
+    return Math.tan(d2r(deg))
+}
+
+export const ra_dec_to_az_alt = (ra: number, dec: number, date: Date, lngLatEl: LngLatEl): [number, number] => {
+    /* Taken from Jean Meeus's Astronomical Algorithms textbook. Using equations
+    13.5 & 13.6*/
+    const hourAngle = (get_gmt(date) + lngLatEl.lng - ra) % 360
+    const tanAzNum = sind(hourAngle)
+    const tanAzDen = cosd(hourAngle) * sind(lngLatEl.lat) - tand(dec) * cosd(lngLatEl.lat)
+    const az = Math.atan2(tanAzNum, tanAzDen) + Math.PI //radians (if reckoning from the north, subtract pi)
+    // const az = Math.atan(tanAzNum/tanAzDen) //radians
+    const sinEl = sind(lngLatEl.lat) * sind(dec) + cosd(lngLatEl.lat) * cosd(dec) * cosd(hourAngle)
+    const el = Math.asin(sinEl) // radians
+    return [r2d(az), r2d(el)]
+}
+
+export const add_hours = (date: Date, hours: number): Date => {
+    const newDate = new Date(date.getTime())
+    newDate.setTime(date.getTime() + hours * 3600000)
+    return newDate
+}
+
+const round_date = (minutes: number, date: Date) => {
+    const coeff = 1000 * 60 * minutes;
+    return new Date(Math.round(date.getTime() / coeff) * coeff)
+}
+
+const round_date_up = (date: Date, minutes: number) => {
+    const coeff = 1000 * 60 * minutes
+    return new Date(Math.ceil(date.getTime() / coeff) * coeff)
+}
+
+const round_date_down = (date: Date, minutes: number) => {
+    const coeff = 1000 * 60 * minutes
+    return new Date(Math.floor(date.getTime() / coeff) * coeff)
+}
+
+export const get_day_times = (startDateTime: Date, endDateTime: Date, stepSize=STEP_SIZE) => {
+    //Used for viz chart
+    const start = round_date_up(startDateTime, stepSize) 
+    const end = round_date_down(endDateTime, stepSize)
+    const nLen = Math.round( ( end.getTime() - start.getTime() ) / (stepSize * 60 * 1000) )
+    let times = Array.from({ length: nLen }, (_, idx) => new Date(start.getTime() + stepSize * 60 * 1000 * idx ))
+    return [start, ...times, end] //and start and end
+}
+
+export const get_times_using_nadir = (nadir: Date, roundMin=ROUND_MINUTES, stepSize=STEP_SIZE) => {
+    const nLen = Math.round( ( TIMES_END - TIMES_START ) / stepSize)
+    const deltaNadir = Array.from({ length: nLen }, (_, idx) => TIMES_START + stepSize * idx )
+    const roundedNadir = round_date(roundMin, nadir)
+    return deltaNadir.map((hour: number) => {
+        return add_hours(roundedNadir, hour)
+    })
+}
+
+
+export const get_target_traj = (ra: number, dec: number, times: Date[], lngLatEl: LngLatEl): [number, number][] => {
+    let traj: [number, number][] = []
+    times.forEach((d: Date) => {
+        traj.push(ra_dec_to_az_alt(ra, dec, d, lngLatEl))
+    })
+    return traj
+}
+
+export function alt_from_air_mass(am: number): number;
+export function alt_from_air_mass(am: number, el: number): number;
+export function alt_from_air_mass(am: number, el?: number) {
+    if (el === undefined) {
+        return 90 - r2d(Math.acos(1 / am))
+    }
+    const a = RADIUS_EARTH + el
+    const b = ATMOSPHERE_HEIGHT + RADIUS_EARTH
+    const s = am * ATMOSPHERE_HEIGHT
+    const zenith = r2d(Math.acos(( a * a + s * s - b * b ) / (2 * a * s)))
+    return 90 - zenith
+}
+
+export function air_mass(alt: number): number; //secant formula
+export function air_mass(alt: number, el: number): number; // Homogeneous spherical atmosphsere with elevated observer
+export function air_mass(alt: number, el?: number) {
+    if (el === undefined) {
+        const zenith = 90 + alt
+        return 1 / cosd(zenith)
+    }
+    const y = el / ATMOSPHERE_HEIGHT
+    const z = RADIUS_EARTH / ATMOSPHERE_HEIGHT
+    const a2 = ATMOSPHERE_HEIGHT * ATMOSPHERE_HEIGHT
+    const r = RADIUS_EARTH + el 
+    const g = ATMOSPHERE_HEIGHT - el 
+    const zenith = 90 - alt
+    const firstTerm = (r * r) * cosd(zenith) * cosd(zenith) / ( a2 )
+    const secondTerm = 2 * RADIUS_EARTH * (g) / a2
+    const thirdTerm = y * y
+    const forthTerm = (y + z) * cosd(zenith)
+    const X = Math.sqrt(firstTerm + secondTerm - thirdTerm + 1) - forthTerm
+    return X
+}
+
+export const get_air_mass = (ra: number, dec: number, times: Date[], lngLatEl: LngLatEl) => {
+    const azAlt = get_target_traj(ra, dec, times, lngLatEl)
+    const airmass = azAlt.map((a: [number, number]) => { return air_mass(a[1], lngLatEl.el) })
+    // const airmass = azAlt.map((a: [number, number]) => { return air_mass(a[1]) })
+    return airmass
+}
+
+export const add_pi = (angle: number) => {
+    return angle + Math.PI
+} 
+
+
+export const get_curr_loc_trace = (targetView: TargetView[], 
+    maxAirmass: number,
+    minAirmass: number,
+    time: Date, 
+    lngLatEl: LngLatEl, 
+    KG: GeoModel, 
+    timezone: string, 
+    date_time_format: string) => {
+    //get curr marker
+    const traces = targetView.map((tgtv: TargetView, idx: number) => { //add current location trace
+            const ra = tgtv.ra_deg as number
+            const dec = tgtv.dec_deg as number
+            const azEl = ra_dec_to_az_alt(ra, dec, time, lngLatEl)
+            const { observable, reasons } = alt_az_observable(azEl[1], azEl[0], KG)
+            const viz: VizRow = {
+                az: azEl[0],
+                alt: azEl[1],
+                datetime: time,
+                observable,
+                reasons,
+            }
+            const datum = viz.alt 
+            const currTime = hidate(time, timezone)
+            const airmass = air_mass(azEl[1], lngLatEl.el)
+            maxAirmass = Math.max(maxAirmass, airmass)
+            minAirmass = Math.min(minAirmass, airmass)
+            let text = `<b>${tgtv.target_name}</b><br>` 
+            text += `Az: ${azEl[0].toFixed(2)}<br>`
+            text += `El: ${azEl[1].toFixed(2)}<br>`
+            text += `HT: ${currTime.format(date_time_format)}`
+
+            const trace: Plotly.Data = {
+                x: [currTime.toDate()],
+                y: [datum],
+                text: [text],
+                hovertemplate: '<b>%{text}</b>', //disable to show xyz coords
+                showlegend: false,
+                marker: {
+                    size: 12,
+                    // color: 'red',
+                    color: colors[idx % colors.length],
+                    line: {
+                        color: 'black',
+                        width: 3
+                    }
+                },
+                textposition: 'top left',
+                type: 'scatter',
+                mode: 'markers',
+                name: tgtv.target_name
+            }
+            return trace
+        })
+    return traces
+}
---
+++ src/dashboard/target_viz_chart.tsx
@@ -0,0 +1,344 @@
+import { GeoModel, useStateContext } from "../App";
+import {
+    MARKER_SIZE,
+    XAXIS_DTICK,
+    MOON_MARKER_LINE_WIDTH,
+    MOON_MARKER_SIZE
+} from "./constants";
+import Plot from "react-plotly.js";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import { BlockReason, DayViz, TargetViz, VizRow } from "./viz_dialog";
+import { air_mass } from "./sky_view_util";
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+
+interface Props {
+    targetViz: TargetViz,
+}
+
+
+export const alt_az_observable = (alt: number, az: number, KG: GeoModel) => {
+    const minDeckAz = KG.t2
+    const maxDeckAz = KG.t3
+    const minAlt = KG.r1
+    const deckAlt = KG.r3
+    const trackLimit = KG.trackLimit
+
+    const reasons: Array<BlockReason> = []
+    //nasdeck is blocking the target?
+    const targetOverlapsDeck = az >= minDeckAz && az <= maxDeckAz
+    const targetBelowDeck = alt >= minAlt && alt <= deckAlt
+    const deckBlocking = targetOverlapsDeck && targetBelowDeck
+    deckBlocking && reasons.push('Deck Blocking')
+
+    //target is below telescope horizon?
+    const targetBelowHorizon = alt < minAlt
+    targetBelowHorizon && reasons.push('Below Horizon')
+
+    //target is above tracking limits?
+    const targetAboveTrackingLimits = alt > trackLimit
+    targetAboveTrackingLimits && reasons.push('Above Tracking Limits')
+
+    const observable = !deckBlocking && !targetBelowHorizon && !targetAboveTrackingLimits
+    return { observable, reasons }
+}
+
+export const date_normalize = (date: Date, utctz = false) => {
+    //if date is before semester date set to next day
+    let out = dayjs(date).set('year', 2000).set('month', 0).set('date', 1)
+    out = out.get('hours') < 12 ? out.add(1, 'day') : out
+    if (utctz) {
+        return out.utc(true).toDate()
+    }
+    return out.toDate()
+}
+
+const colors = {
+    'Deck Blocking': '#7570b3',
+    'Below Horizon': '#e7298a',
+    'Above Tracking Limits': '#d95f02'
+}
+
+export const reason_to_color_mapping = (reasons: string[]) => {
+    const cols = reasons.map((reason: string) => colors[reason as keyof typeof colors])
+    return cols.length ? cols[0] : '#1b9e77'
+}
+
+const create_dawn_dusk_text = (date: Date, date_time_format: string) => {
+    let txt = ""
+    txt += "HT: " + dayjs(date).format(date_time_format) + "<br>"
+    txt += "UT: " + dayjs(date).utc(false).format(date_time_format)
+    return txt
+}
+
+export const get_browser_name = () => {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.includes("Chrome")) {
+        return "Chrome";
+    } else if (userAgent.includes("Firefox")) {
+        return "Firefox";
+    } else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+        return "Safari";
+    } else if (userAgent.includes("Edg")) {
+        return "Microsoft Edge";
+    } else if (userAgent.includes("Opera") || userAgent.includes("Opr")) {
+        return "Opera";
+    } else {
+        return "Unknown Browser";
+    }
+}
+
+
+export const create_dawn_dusk_traces = (targetViz: TargetViz, date_time_format: string) => {
+
+    const browserName = get_browser_name();
+    // scattergl is a hack which is needed to overlap markers
+    // Safari disables WebGL by default, so just let it be scatter.
+    const chart_type = browserName === "Safari" ? 'scatter' : 'scattergl'
+    const trace = {
+        yaxis: 'y2',
+        textposition: 'top left',
+        mode: 'lines+markers',
+        showlegend: false,
+        type: chart_type,
+        marker: {
+            size: MOON_MARKER_SIZE,
+            symbol: 'square',
+        },
+        line: {
+            width: MOON_MARKER_LINE_WIDTH
+        }
+    }
+    let dawn_dusk_traces: { [key: string]: any } = {
+        dusk: {
+            ...trace,
+            x: [],
+            y: [],
+            text: [],
+            name: 'Nautical Dusk (6 deg)',
+            marker: {
+                ...trace.marker,
+                color: '#eeeeee',
+            },
+            line: {
+                ...trace.line,
+                color: '#eeeeee',
+            }
+        },
+        amateur_dusk: {
+            ...trace,
+            x: [],
+            y: [],
+            text: [],
+            name: 'Amateur Dusk (12 deg)',
+            marker: {
+                ...trace.marker,
+                color: '#dddddd',
+            },
+            line: {
+                ...trace.line,
+                color: '#dddddd',
+            }
+        },
+        astronomical_dusk: {
+            ...trace,
+            x: [],
+            y: [],
+            text: [],
+            name: 'Astronomical Dusk (18 deg)',
+            marker: {
+                ...trace.marker,
+                color: '#cccccc',
+            },
+            line: {
+                ...trace.line,
+                color: '#cccccc',
+            }
+        },
+        astronomical_dawn: {
+            ...trace,
+            x: [],
+            y: [],
+            text: [],
+            name: 'Astronomical Dawn (18 deg)',
+            marker: {
+                ...trace.marker,
+                color: '#cccccc',
+            },
+            line: {
+                ...trace.line,
+                color: '#cccccc',
+            }
+        },
+        amateur_dawn: {
+            ...trace,
+            x: [],
+            y: [],
+            text: [],
+            name: 'Amateur Dawn (12 deg)',
+            marker: {
+                ...trace.marker,
+                color: '#dddddd',
+            },
+            line: {
+                ...trace.line,
+                color: '#dddddd',
+            }
+        },
+        dawn: {
+            ...trace,
+            x: [],
+            y: [],
+            text: [],
+            name: 'Nautical Dawn (6 deg)',
+            marker: {
+                ...trace.marker,
+                color: '#eeeeee',
+            },
+            line: {
+                ...trace.line,
+                color: '#eeeeee',
+            }
+        }
+    }
+
+    targetViz.semester_visibility.forEach((dayViz: DayViz) => {
+        const xdate = new Date(dayjs(dayViz.date).format('YYYY-MM-DD'))
+        dawn_dusk_traces.amateur_dawn.x.push(xdate)
+        dawn_dusk_traces.amateur_dusk.x.push(xdate)
+        dawn_dusk_traces.dawn.x.push(xdate)
+        dawn_dusk_traces.dusk.x.push(xdate)
+        dawn_dusk_traces.astronomical_dawn.x.push(xdate)
+        dawn_dusk_traces.astronomical_dusk.x.push(xdate)
+
+        dawn_dusk_traces.amateur_dawn.y.push(date_normalize(dayViz.amateurDawn, true))
+        dawn_dusk_traces.amateur_dusk.y.push(date_normalize(dayViz.amateurDusk, true))
+        dawn_dusk_traces.dawn.y.push(date_normalize(dayViz.dawn, true))
+        dawn_dusk_traces.dusk.y.push(date_normalize(dayViz.dusk, true))
+        dawn_dusk_traces.astronomical_dawn.y.push(date_normalize(dayViz.astronomicalDawn, true))
+        dawn_dusk_traces.astronomical_dusk.y.push(date_normalize(dayViz.astronomicalDusk, true))
+
+        dawn_dusk_traces.amateur_dawn.text.push(create_dawn_dusk_text(dayViz.amateurDawn, date_time_format))
+        dawn_dusk_traces.amateur_dusk.text.push(create_dawn_dusk_text(dayViz.amateurDusk, date_time_format))
+        dawn_dusk_traces.dawn.text.push(create_dawn_dusk_text(dayViz.amateurDusk, date_time_format))
+        dawn_dusk_traces.dusk.text.push(create_dawn_dusk_text(dayViz.amateurDusk, date_time_format))
+        dawn_dusk_traces.astronomical_dawn.text.push(create_dawn_dusk_text(dayViz.amateurDusk, date_time_format))
+        dawn_dusk_traces.astronomical_dusk.text.push(create_dawn_dusk_text(dayViz.amateurDusk, date_time_format))
+    })
+    return dawn_dusk_traces
+}
+
+export const TargetVizChart = (props: Props) => {
+    const { targetViz } = props
+    console.log('TargetVizChart', targetViz)
+    const context = useStateContext()
+    let traces = targetViz.semester_visibility.map((dayViz: DayViz) => {
+        let texts: string[] = []
+        let y: Date[] = []
+        let color: string[] = []
+        //let color: number[] = []
+        dayViz.visibility.forEach((viz: VizRow) => {
+            let text = `<b>${targetViz.target_name} Visibility</b><br>` 
+            text += `Az: ${viz.az.toFixed(2)}<br>`
+            text += `El: ${viz.alt.toFixed(2)}<br>`
+            text += `Airmass: ${air_mass(viz.alt, context.config.tel_lat_lng_el.keck.el).toFixed(2)}<br>`
+            // text += `Airmass: ${air_mass(viz.alt).toFixed(2)}<br>`
+            text += `HT: ${dayjs(viz.datetime).format(context.config.date_time_format)}<br>`
+            text += `UT: ${dayjs(viz.datetime).utc(false).format(context.config.date_time_format)}<br>`
+            text += `Moon Fraction: ${viz.moon_illumination.fraction.toFixed(2)}<br>`
+            text += `Visible for: ${dayViz.visible_hours.toFixed(2)} hours<br>`
+            text += viz.observable ? '' : `<br>Not Observable: ${viz.reasons.join(', ')}`
+
+
+            color.push(reason_to_color_mapping(viz.reasons))
+            const daytime = date_normalize(viz.datetime)
+            y.push(daytime)
+            texts.push(text)
+        })
+        const ydate = new Date(dayjs(dayViz.date).format('YYYY-MM-DD'))
+        const x = Array.from({ length: y.length }, () => ydate)
+
+        const trace: Partial<Plotly.PlotData> = {
+            x,
+            y,
+            text: texts,
+            marker: {
+                color,
+                size: MARKER_SIZE,
+                symbol: 'square',
+                opacity: 1 // too dense to see ticks
+            },
+            hovertemplate: '<b>%{text}</b> <extra></extra>', //disable to show xyz coords
+            line: {
+                width: 0,
+            },
+            textposition: 'top left',
+            type: 'scattergl',
+            mode: 'lines+markers',
+            showlegend: false,
+            name: targetViz.target_name ?? 'Target'
+        }
+        return trace
+    })
+
+    const lightTraces = Object.values(create_dawn_dusk_traces(targetViz, context.config.date_time_format)) as Plotly.PlotData[]
+    //@ts-ignore
+    traces = [...traces, ...lightTraces]
+    let titleText = targetViz.target_name ?? 'Target' 
+    titleText += ' Visibility'
+    titleText = `<b>${titleText}</b>`
+
+    const layout: Partial<Plotly.Layout> = {
+        width: 1200,
+        height: 400,
+        title: { text: titleText },
+        plot_bgcolor: 'black',
+        yaxis2: {
+            title: { text: 'Time [UT]' },
+            type: 'date',
+            gridwidth: 0,
+            overlaying: 'y',
+            // scaleanchor: 'y',
+            side: 'right',
+            layer: 'above traces',
+            autorange: 'reversed',
+            // tickmode: 'auto',
+            tickformat: '%H:%M',
+        },
+        yaxis: {
+            title: { text: 'Time [HT]' },
+            type: 'date',
+            gridwidth: 2,
+            gridcolor: 'white',
+            // scaleanchor: 'y2',
+            autorange: 'reversed',
+            layer: 'above traces',
+            // tickmode: 'auto',
+            tickformat: '%H:%M',
+        },
+        xaxis: {
+            title: { text: 'Date' },
+            type: 'date',
+            gridwidth: 2,
+            gridcolor: 'white',
+            layer: 'above traces',
+            dtick: XAXIS_DTICK,
+            tickformat: '%Y-%m-%d',
+            tickmode: 'auto',
+            //nticks: 0
+        },
+        // hovermode: "closest",
+    }
+
+    return (
+        <Plot
+            data={traces}
+            divId='target-viz-chart'
+            layout={layout}
+        />
+    )
+}
