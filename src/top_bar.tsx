7d4df1cf0be446f2754509446f9e67329ead9c44
Tyler Tucker <tylertucker202@gmail.com>
2025-09-30 06:23:46 -1000

    adding dropdown menu for module selection

--- src/top_bar.tsx
+++ src/top_bar.tsx
@@ -42,7 +42,7 @@ export default function KPFModuleSelect(props: KPFModuleSelectProps) {
     <Tooltip
       placement="right"
       title="Select KPF Module. Either the Observing Block Webform for requesting/submitting OBS or viewing the Dashboard">
-      <Box sx={{ minWidth: 120 }}>
+      <Box sx={{ minWidth: 120, height: 30 }}>
         <FormControl fullWidth>
           <InputLabel id="select-kpf-module-label">KPF Module</InputLabel>
           <Select
