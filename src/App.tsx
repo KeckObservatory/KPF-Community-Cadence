8676c931600cce9d28f2f20b5f765195a475d0aa
Tyler Tucker <tylertucker202@gmail.com>
2025-09-30 06:40:49 -1000

    adding dropdown menu for module selection

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import Alert from '@mui/material/Alert';
 import { LicenseInfo } from '@mui/x-license';
 import licenseKey from './license.json'
 import { ComponentSelector, OB } from './component_selector';
-import { Dashboard } from './dashboard/dashboard_button';
 
 
 
@@ -300,7 +299,7 @@ function App() {
                   module === 'webform' &&
                   <>
                     (<Control notApproved={notApproved} isAdmin={isAdmin} />
-                    init ?  <ComponentSelector /> : <Skeleton variant="rectangular" width="100%" height={500} />)
+                    {init} ?  <ComponentSelector /> : <Skeleton variant="rectangular" width="100%" height={500} />)
                   </>
                 }
                 {module === 'dashboard' && (<Skeleton variant="rectangular" width="100%" height={500} />)}
