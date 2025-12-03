4eead7d7927b879dde3fdb2fabab9de2e235ca5d
Tyler Coda <tylertucker202@gmail.com>
2025-12-03 07:33:35 -1000

    Remove React.StrictMode from main.tsx
    Removed React.StrictMode wrapping from the render method.

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.Fragment>
-    {/* <React.StrictMode> */}
       <BrowserRouter>
         <QueryParamProvider adapter={ReactRouter6Adapter}>
           <App />
         </QueryParamProvider>
       </BrowserRouter>
-    {/* </React.StrictMode> */}
   </React.Fragment>
 )
