e1620fc79451de4fb6ebe82067033aa080a47569
Tyler Tucker <tylertucker202@gmail.com>
2026-02-10 11:07:40 -1000

    column order matters!

--- src/ob_component_table.tsx
+++ src/ob_component_table.tsx
@@ -237,6 +237,10 @@ export default function OBComponentTable(props: Props) {
     if (componentName.includes('target')) {
         columns = [target_name_col, target_name_semid_col, ...columns.slice(1), ]
     }
+    else {
+        columns = [target_name_col, target_name_semid_col, ...columns]
+    }
+
     if (componentName.includes('schedule')) {
         pinnedColumns.right?.push('ob_feasible')
         columns = [...columns, ob_feasible_col]
